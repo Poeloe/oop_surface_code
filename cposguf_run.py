f713f669633d17b84e3c26b709056f2ce7d92408
Mark <watermarkhu@gmail.com>
2019-09-29 22:18:10 +0200

    small fix for sim2

--- cposguf_run.py
+++ cposguf_run.py
@@ -7,20 +7,8 @@ from progiter import ProgIter
 import psycopg2 as pgs
 from psycopg2 import extras as pgse
 from configparser import ConfigParser
-import random
 import cpuinfo
 import time
-
-
-
-def add_new_sim(cursor, comp_id, lattice, p, ubuck, vcomb, array):
-    query = "INSERT INTO simulations (lattice, p, comp_id, created_on, ubuck_solved, vcomb_solved, error_data) VALUES ({0}, {1}, '{2}', current_timestamp, {3}, {4}, ARRAY{5});".format(lattice, p, comp_id, ubuck, vcomb, array)
-    cursor.execute(query)
-
-
-def add_new_comp(cursor, id, cputype):
-    query = "INSERT INTO computers (comp_id, cpu_type) VALUES ('" + id + "', '" + cputype + "');"
-    cursor.execute(query)
 
 
 def read_config(path):
@@ -36,6 +24,28 @@ def read_config(path):
     return comp_id, num_process, iters, sql_config
 
 
+def sql_connection(ini_path=None, get_data=False):
+
+    data = read_config("./cposguf.ini") if ini_path is None else read_config(ini_path)
+    con = pgs.connect(**data[3])
+    con.set_session(autocommit=True)
+    cur = con.cursor()
+    if get_data:
+        return con, cur, data
+    else:
+        return con, cur
+
+
+def add_new_sim(cursor, comp_id, lattice, p, ubuck, vcomb, array):
+    query = "INSERT INTO simulations (lattice, p, comp_id, created_on, ubuck_solved, vcomb_solved, error_data) VALUES ({0}, {1}, '{2}', current_timestamp, {3}, {4}, ARRAY{5});".format(lattice, p, comp_id, ubuck, vcomb, array)
+    cursor.execute(query)
+
+
+def add_new_comp(cursor, id, cputype):
+    query = "INSERT INTO computers (comp_id, cpu_type) VALUES ('" + id + "', '" + cputype + "');"
+    cursor.execute(query)
+
+
 def output_error_array(graph):
     array = [[], [], []]
     for edge in graph.E.values():
@@ -48,10 +58,53 @@ def output_error_array(graph):
 
 def input_error_array(graph, array):
     for y, x, td in list(map(list, zip(*array))):
-        graph.E[(0, y, x, td)].state = 1
+        graph.E[(0, int(y), int(x), int(td))].state = 1
 
 
-def multiple(ini, comp_id, iters, size, p, worker=None):
+def fetch_query(selection, p=None, L=None, type=None, limit=None, extra=None):
+    query = "SELECT {} FROM simulations ".format(selection)
+    if L is not None:
+        if isinstance(L, int):
+            query += "WHERE lattice = " + str(L) + " "
+        elif isinstance(L, list):
+            query += "WHERE lattice IN " + str(L) + " "
+        if any([p is not None, type is not None, extra is not None]):
+            query += "AND "
+
+    if p is not None:
+        if L is None:
+            query += "WHERE "
+        if isinstance(p, float):
+            query += "p = " + str(p) + " "
+        elif isinstance(p, list):
+            query += "p IN " + str(p) + " "
+        if type is not None or extra is not None:
+            query += "AND "
+
+    if type is not None:
+        if L is None and p is None:
+            query += "WHERE "
+        if type == "ubuck":
+            query += "ubuck_solved = TRUE "
+        elif type == "vcomb":
+            query += "vcomb_solved = TRUE "
+        if extra is not None:
+            query += "AND "
+
+    if extra is not None:
+        if all([L is None, p is None, type is None]):
+            query += "WHERE "
+        query += extra
+        if extra[-1] != " ":
+            query += " "
+
+    if limit is not None:
+        query += "LIMIT {}".format(limit)
+
+    return query
+
+
+def multiple(comp_id, iters, size, p, worker=None):
 
     def single(graph):
         te.init_pauli(graph, pX=p, worker=worker)           # Simulate for unique bucket method
@@ -78,9 +131,7 @@ def multiple(ini, comp_id, iters, size, p, worker=None):
         return (ubuck_win, vcomb_win, array)
 
     # Open SQL connection
-    con = pgs.connect(**ini)
-    con.set_session(autocommit=True)
-    cur = con.cursor()
+    con, cur = sql_connection()
 
     # Simulate
     graph = go.init_toric_graph(size)
@@ -112,7 +163,7 @@ def multiple(ini, comp_id, iters, size, p, worker=None):
     con.close()
 
 
-def multiprocess(ini, comp_id, iters, size, p, processes=None):
+def multiprocess(comp_id, iters, size, p, processes=None):
 
     if processes is None:
         processes = mp.cpu_count()
@@ -120,9 +171,11 @@ def multiprocess(ini, comp_id, iters, size, p, processes=None):
     process_iters = iters//processes
     rest_iters = iters - process_iters*processes
     workers = []
+
     for i in range(processes-1):
-        workers.append(mp.Process(target=multiple, args=(ini, comp_id, process_iters, size, p, i)))
-    workers.append(mp.Process(target=multiple, args=(ini, comp_id, process_iters + rest_iters, size, p, processes - 1)))
+        workers.append(mp.Process(target=multiple, args=(comp_id, process_iters, size, p, i)))
+    workers.append(mp.Process(target=multiple, args=(comp_id, process_iters + rest_iters, size, p, processes - 1)))
+
 
     for worker in workers:
         worker.start()
@@ -135,11 +188,7 @@ def multiprocess(ini, comp_id, iters, size, p, processes=None):
 
 if __name__ == '__main__':
 
-    comp_id, num_process, iters, sql_config = read_config("./cposguf.ini")
-
-    con = pgs.connect(**sql_config)
-    con.set_session(autocommit=True)
-    cur = con.cursor()
+    con, cur, (comp_id, num_process, iters, sql_config) = sql_connection(get_data=1)
 
     # Check computer listing
     cur.execute("SELECT comp_id FROM computers")
@@ -150,25 +199,25 @@ if __name__ == '__main__':
 
     running = True
     while True:
-
         # Choose case
-        cur.execute("SELECT lattice,p FROM cases_open_free")
-        options = cur.fetchall()
-        if len(options) == 0:
-            cur.execute("SELECT lattice,p FROM cases_open")
-            options = cur.fetchall()
-            if len(options) == 0:
+        cur.execute("SELECT lattice, p FROM cases_open_free ORDER BY progress LIMIT 1")
+        lowest = cur.fetchone()
+        if lowest == None:
+            cur.execute("SELECT lattice, p FROM cases_open ORDER BY progress LIMIT 1")
+            lowest = cur.fetchone()
+            if lowest == None:
                 exit()
-        lattice, deci_p = random.choice(options)
+        lattice, deci_p = lowest
+
         p = float(deci_p)
         print("Selected lattice = {}, p = {}".format(lattice, p))
 
         # Set computer active case and simulate
         cur.execute("UPDATE computers SET active_lattice = {}, active_p = {} WHERE comp_id = '{}'".format(lattice, p, comp_id))
         if num_process == 1:
-            multiple(sql_config, comp_id, iters, lattice, p)
+            multiple(comp_id, iters, lattice, p)
         else:
-            multiprocess(sql_config, comp_id, iters, lattice, p, num_process)
+            multiprocess(comp_id, iters, lattice, p, num_process)
 
         # Check if keep running
         cur.execute("SELECT active_lattice, active_p FROM computers WHERE comp_id = '{}'".format(comp_id))
