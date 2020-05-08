177388e3bf4825df5bab97fc8c9988572f9c1514
Paul <paul_707070@hotmail.com>
2020-05-08 18:28:02 +0200

    Fixed bugs in both fidelity functions and added minimal documentation to all the functions

--- circuit_simulation/basic_operations.py
+++ circuit_simulation/basic_operations.py
@@ -1,10 +1,12 @@
 from circuit_simulation.states_and_gates import *
 import numpy as np
 from scipy import sparse as sp
+from scipy.linalg import sqrtm
 import random
 
 
 def state_repr(state):
+    """ Returns the visual representation of the given state if known """
     if np.array_equal(state, ket_0):
         return "|0>"
     if np.array_equal(state, ket_1):
@@ -16,6 +18,7 @@ def state_repr(state):
 
 
 def gate_name(gate):
+    """ Returns the (visual) representation of the given gate if known """
     if np.array_equal(gate, X):
         return "X"
     if np.array_equal(gate, Y):
@@ -30,6 +33,7 @@ def gate_name(gate):
 
 
 def gate_name_to_array(gate_name):
+    """ Translates the (visual) representation of the given gate to the corresponding matrix if known """
     if gate_name == "X":
         return X
     if gate_name == "Y":
@@ -44,6 +48,7 @@ def gate_name_to_array(gate_name):
 
 
 def get_value_by_prob(array, p):
+    """ Returns, bases on the given weights 'p', a value out of the given array """
     r = random.random()
     index = 0
     while r >= 0 and index < len(p):
@@ -53,6 +58,7 @@ def get_value_by_prob(array, p):
 
 
 def KP(*args):
+    """ Returns the Kronecker product of the given arguments in the exact order """
     result = None
     for state in args:
         if state is None:
@@ -65,18 +71,26 @@ def KP(*args):
 
 
 def CT(state1, state2=None):
+    """ returns the dot prodcut of the two passed states, where the second state will be the conjugate transpose """
     state2 = state1 if state2 is None else state2
     return sp.csr_matrix(state1).dot(sp.csr_matrix(state2).conj().T)
 
 
 def trace(sparse_matrix):
+    """ Returns the trace of a matrix"""
     if not sp.issparse(sparse_matrix):
         sp.csr_matrix(sparse_matrix)
 
-    return sparse_matrix.diagonal().sum()
+    result = sparse_matrix.diagonal().sum()
+
+    if isinstance(result, complex):
+        result = result.real
+
+    return result
 
 
 def N_dim_ket_0_or_1_density_matrix(N, ket=0):
+    """ Returns an N-qubit version of the ket_0 or ket_1 density matrix """
     dim = 2**N
     rho = sp.lil_matrix((dim, dim))
     if ket == 1:
@@ -87,22 +101,23 @@ def N_dim_ket_0_or_1_density_matrix(N, ket=0):
 
 
 def fidelity(rho, sigma):
+    """ Calculates the fidelity of two density matrices according to the 'classical' method """
     if not sp.issparse(rho):
         rho = sp.csr_matrix(rho)
     if not sp.issparse(sigma):
         sigma = sp.csr_matrix(sigma)
 
-    rho_root = sp.csr_matrix(np.sqrt(rho.toarray()))
-    resulting_matrix = np.sqrt((rho_root * sigma * rho_root).toarray())
+    rho_root = sp.csr_matrix(sqrtm(rho.toarray()))
+    resulting_matrix = sqrtm((rho_root * (sigma * rho_root)).toarray())
     return (trace(resulting_matrix))**2
 
 
 def fidelity_elementwise(rho, sigma):
+    """ Calculates the fidelity using the element wise multiplication method """
     if not sp.issparse(rho):
-        rho = sp.csr_matrix(rho)
+        rho = rho.toarray()
     if not sp.issparse(sigma):
-        sigma = sp.csr_matrix(sigma)
+        sigma = sigma.toarray()
 
-    rho_root = np.sqrt(rho.toarray())
-    resulting_matrix = np.sqrt(rho_root * sigma.toarray() * rho_root)
-    return trace(resulting_matrix)**2
+    resulting_matrix = rho * sigma * rho
+    return trace(resulting_matrix)
