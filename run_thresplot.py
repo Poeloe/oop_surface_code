49abc6318ea0244dedd2b86f82ff37aeb9912a1d
Paul <paul_707070@hotmail.com>
2020-06-29 09:35:22 +0200

    Solved bug that prevented multithreading from working

--- run_thresplot.py
+++ run_thresplot.py
@@ -9,27 +9,28 @@ import argparse
 from run_oopsc import add_args, add_kwargs
 from oopsc.threshold.plot import plot_thresholds
 
+if __name__ == "__main__":
 
-parser = argparse.ArgumentParser(
-    prog="threshold_fit",
-    description="fit a threshold computation",
-    usage='%(prog)s [-h/--help] file_name'
-)
+    parser = argparse.ArgumentParser(
+        prog="threshold_fit",
+        description="fit a threshold computation",
+        usage='%(prog)s [-h/--help] file_name'
+    )
 
-arguments = [["file_name", "store", str, "file name of csv data (without extension)", "file_name", dict()]]
+    arguments = [["file_name", "store", str, "file name of csv data (without extension)", "file_name", dict()]]
 
-key_arguments = [
-    ["-p", "--probs", "store", "p items to plot - verbose list", dict(type=float, nargs='*', metavar="")],
-    ["-l", "--latts", "store", "L items to plot - verbose list", dict(type=float, nargs='*', metavar="")],
-    ["-ma", "--modified_ansatz", "store_true", "use modified ansatz - toggle", dict()],
-    ["-o", "--output", "store", "output file name", dict(type=str, default="", metavar="")],
-    ["-pt", "--plot_title", "store", "plot filename", dict(type=str, default="", metavar="")],
-    ["-ymin", "--ymin", "store", "limit yaxis min", dict(type=float, default=0.5, metavar="")],
-    ["-ymax", "--ymax", "store", "limit yaxis max", dict(type=float, default=1, metavar="")],
+    key_arguments = [
+        ["-p", "--probs", "store", "p items to plot - verbose list", dict(type=float, nargs='*', metavar="")],
+        ["-l", "--latts", "store", "L items to plot - verbose list", dict(type=float, nargs='*', metavar="")],
+        ["-ma", "--modified_ansatz", "store_true", "use modified ansatz - toggle", dict()],
+        ["-o", "--output", "store", "output file name", dict(type=str, default="", metavar="")],
+        ["-pt", "--plot_title", "store", "plot filename", dict(type=str, default="", metavar="")],
+        ["-ymin", "--ymin", "store", "limit yaxis min", dict(type=float, default=0.5, metavar="")],
+        ["-ymax", "--ymax", "store", "limit yaxis max", dict(type=float, default=1, metavar="")],
 
-]
+    ]
 
-add_args(parser, arguments)
-add_kwargs(parser, key_arguments)
-args=vars(parser.parse_args())
-plot_thresholds(**args)
+    add_args(parser, arguments)
+    add_kwargs(parser, key_arguments)
+    args=vars(parser.parse_args())
+    plot_thresholds(**args)
