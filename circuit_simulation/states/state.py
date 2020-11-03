f82c6fb5be6cc7812396e097e06abb297f08f439
Paul <paul_707070@hotmail.com>
2020-11-03 12:53:09 +0100

    Made state vector calculation possible

--- circuit_simulation/states/state.py
+++ circuit_simulation/states/state.py
@@ -1,5 +1,7 @@
 import scipy.sparse as sp
 import numpy as np
+import re
+
 
 class State(object):
     """
@@ -37,6 +39,7 @@ class State(object):
 
     @property
     def sp_vector(self):
+        self._sp_vector.eliminate_zeros()
         return self._sp_vector
 
     def __repr__(self):
@@ -46,3 +49,26 @@ class State(object):
         if type(other) != State:
             return False
         return np.array_equal(self.vector, other.vector)
+
+    def __rmul__(self, other):
+        if type(other) not in [int, float]:
+            return ValueError("Not allowed! Please multiply by int or float")
+        return State(self.name, other * self.vector, '{}({})'.format(other, self.representation))
+
+    def __mul__(self, other):
+        if type(other) != State:
+            raise ValueError("Not allowed! If multiplying by a factor is wanted, please put it in front.")
+        pattern = re.compile('(?<=\|)(.*)(?=\>)')
+        first_repr = re.search(pattern, self.representation).group(1)
+        second_repr = re.search(pattern, other.representation).group(1)
+        vector = sp.kron(self.sp_vector, other.sp_vector)
+        vector.eliminate_zeros()
+        return State(self.name + other.name,
+                     vector,
+                     "|{}{}>".format(first_repr, second_repr))
+
+    def __add__(self, other):
+        vector = self.sp_vector + other.sp_vector
+        repr_state = '{} + {}'.format(self.representation, other.representation)
+        return State(repr_state, vector, repr_state)
+
