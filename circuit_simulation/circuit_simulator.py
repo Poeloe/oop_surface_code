512e3be0da6bcd852cff1d222b1ea3416e1babe6
sebastiandebone <s.w.debone@tudelft.nl>
2020-11-12 12:23:44 +0100

    finished first draft of protocol with 14 Bell pairs

--- circuit_simulation/circuit_simulator.py
+++ circuit_simulation/circuit_simulator.py
@@ -1541,12 +1541,13 @@ class QuantumCircuit:
                 success = True
 
     @skip_if_cut_off_reached
-    def single_selection_var(self, operation1, operation2, bell_qubit_1, bell_qubit_2, measure=True, noise=None, pn=None, pm=None,
-                         pg=None, retry=True, user_operation=True):
+    def single_selection_var(self, operation1, operation2, bell_qubit_1, bell_qubit_2, create_bell_pair=True,
+                             measure=True, noise=None, pn=None, pm=None, pg=None, retry=True, user_operation=True):
         """ Single selection as specified by Naomi Nickerson in https://www.nature.com/articles/ncomms2773.pdf """
         success = False
         while not success:
-            self.create_bell_pair(bell_qubit_1, bell_qubit_2, noise=noise, pn=pn, user_operation=user_operation)
+            if create_bell_pair:
+                self.create_bell_pair(bell_qubit_1, bell_qubit_2, noise=noise, pn=pn, user_operation=user_operation)
             self.apply_gate(operation1, cqubit=bell_qubit_1, tqubit=bell_qubit_1 + 1, noise=noise, pg=pg,
                             user_operation=user_operation)
             self.apply_gate(operation2, cqubit=bell_qubit_2, tqubit=bell_qubit_2 + 1, noise=noise, pg=pg,
--- circuit_simulation/stabilizer_measurement_protocols/stabilizer_measurement_protocols.py
+++ circuit_simulation/stabilizer_measurement_protocols/stabilizer_measurement_protocols.py
@@ -38,6 +38,31 @@ def create_quantum_circuit(protocol, *, pg, pm, pm_1, pn, decoherence, bell_pair
         qc.define_sub_circuit("AB", [0, 1, 2, 6, 8, 3, 4, 5, 10, 12], waiting_qubits=[6, 8, 10, 12])
 
         return qc
+    elif protocol == 'dyn_prot_14_1':
+        qc = QuantumCircuit(22, 2, noise=True, basis_transformation_noise=False, pg=pg, pm=pm, pm_1=pm_1, pn=pn,
+                            thread_safe_printing=True, probabilistic=probabilistic, T1_lde=2,
+                            decoherence=decoherence, p_bell_success=bell_pair_creation_success, T1_idle=(5 * 60),
+                            T2_idle=10, T2_idle_electron=1, T2_lde=2, measurement_duration=measurement_duration,
+                            bell_creation_duration=bell_pair_creation_duration, pulse_duration=pulse_duration,
+                            single_qubit_gate_lookup=lkt_1q, two_qubit_gate_lookup=lkt_2q, T1_idle_electron=1000,
+                            no_single_qubit_error=True, fixed_lde_attempts=fixed_lde_attempts,
+                            network_noise_type=network_noise_type)
+
+        qc.define_node("A", qubits=[20, 13, 12, 11, 10], electron_qubits=10, data_qubits=20)
+        qc.define_node("B", qubits=[18, 9, 8, 7], electron_qubits=7, data_qubits=18)
+        qc.define_node("C", qubits=[16, 6, 5, 4, 3], electron_qubits=3, data_qubits=16)
+        qc.define_node("D", qubits=[14, 2, 1, 0], electron_qubits=0, data_qubits=14)
+
+        qc.define_sub_circuit("AB", [13, 12, 11, 10, 9, 8, 7, 20, 18], waiting_qubits=[12, 8, 20, 18])
+        qc.define_sub_circuit("CD", [6, 5, 4, 3, 2, 1, 0, 16, 14], waiting_qubits=[5, 1, 16, 14],
+                              concurrent_sub_circuits="AB")
+        qc.define_sub_circuit("AC", [13, 6, 12, 11, 10, 5, 4, 3, 20, 16], waiting_qubits=[12, 5, 20, 16])
+        qc.define_sub_circuit("BD", [9, 2, 8, 7, 1, 0, 18, 14], waiting_qubits=[8, 1, 18, 14],
+                              concurrent_sub_circuits="AC")
+        qc.define_sub_circuit("A", [20, 13, 12, 11, 10])
+        qc.define_sub_circuit("B", [18, 9, 8, 7])
+        qc.define_sub_circuit("C", [16, 6, 5, 4, 3])
+        qc.define_sub_circuit("D", [14, 2, 1, 0], concurrent_sub_circuits=["A", "B", "C"])
     else:
         qc = QuantumCircuit(20, 2, noise=True, basis_transformation_noise=False, pg=pg, pm=pm, pm_1=pm_1, pn=pn,
                             thread_safe_printing=True, probabilistic=probabilistic, T1_lde=2,
@@ -193,76 +218,97 @@ def dyn_prot_14_1(qc, *, operation, color, save_latex_pdf, pbar, draw_circuit, t
         qc.start_sub_circuit("AB")
         success_ab = False
         while not success_ab:
-            qc.create_bell_pair(11, 8)
-            success_ab = qc.single_selection(CZ_gate, 10, 7, retry=False)
+            qc.create_bell_pair(13, 9)
+            success_ab = qc.single_selection(CZ_gate, 12, 8, retry=False)
             if not success_ab:
                 continue
             success_ab2 = False
             while not success_ab2:
-                qc.create_bell_pair(10, 7)
-                success_ab2 = qc.single_selection(CNOT_gate, 9, 6, retry=False)
+                qc.create_bell_pair(12, 8)
+                success_ab2 = qc.single_selection(CNOT_gate, 11, 7, retry=False)
                 if not success_ab2:
                     continue
-            success_ab = qc.single_selection_var(CY_gate, CminY_gate, 10, 7, retry=False)
+            success_ab = qc.single_selection_var(CY_gate, CminY_gate, 12, 8, create_bell_pair=False, retry=False)
 
         pbar.update(20) if pbar is not None else None
 
         qc.start_sub_circuit("CD")
         success_cd = False
         while not success_cd:
-            qc.create_bell_pair(5, 2)
-            success_cd = qc.single_selection(CZ_gate, 4, 1, retry=False)
+            qc.create_bell_pair(6, 2)
+            success_cd = qc.single_selection(CZ_gate, 5, 1, retry=False)
             if not success_cd:
                 continue
             success_cd2 = False
             while not success_cd2:
-                qc.create_bell_pair(4, 1)
-                success_cd2 = qc.single_selection(CZ_gate, 3, 0, retry=False)
+                qc.create_bell_pair(5, 1)
+                success_cd2 = qc.single_selection(CZ_gate, 4, 0, retry=False)
                 if not success_cd2:
                     continue
-            success_cd = qc.single_selection(CNOT_gate, 4, 1, retry=False)
+            success_cd = qc.single_selection_var(CNOT_gate, CNOT_gate, 5, 1, create_Bell_pair=False, retry=False)
 
         pbar.update(20) if pbar is not None else None
 
-        # Step 3-5 from Table D.1 (Thesis Naomi Nickerson)
         qc.start_sub_circuit("AC")
-        # Return success (even parity measurement). If False (uneven), X-gate must be drawn at second single dot
-        success_1 = qc.single_dot(CZ_gate, 10, 4, parity_check=False)
-        qc.start_sub_circuit("BD")
-        ghz_success = qc.single_dot(CZ_gate, 7, 1, draw_X_gate=not success_1, retry=False)
-        if not ghz_success:
-            continue
+        success_ac = False
+        while not success_ac:
+            success_ac2 = False
+            while not success_ac2:
+                qc.create_bell_pair(11, 4)
+                success_ac2 = qc.single_selection(CNOT_gate, 10, 3, retry=False)
+            qc.create_bell_pair(12, 5)
+            success_ac = qc.single_selection_var(CY_gate, CminY_gate, 11, 4, create_bell_pair=False, retry=False)
+            if not success_ac:
+                continue
+            success_ac = qc.single_selection(CZ_gate, 11, 4, retry=False)
 
-        pbar.update(20) if pbar is not None else None
-
-        # Step 6-8 from Table D.1 (Thesis Naomi Nickerson)
-        qc.start_sub_circuit("AC", forced_level=True)
-        ghz_success_1 = qc.single_dot(CZ_gate, 10, 4, retry=False)
         qc.start_sub_circuit("BD")
-        ghz_success_2 = qc.single_dot(CZ_gate, 7, 1, retry=False)
-        if any([not ghz_success_1, not ghz_success_2]):
+        success_bd = False
+        while not success_bd:
+            qc.create_bell_pair(8, 1)
+            success_bd = qc.single_selection(CZ_gate, 7, 0, retry=False)
+
+        qc.start_sub_circuit("A")
+        qc.apply_gate(CNOT_gate, cqubit=13, tqubit=12)
+        qc.start_sub_circuit("C")
+        qc.apply_gate(CNOT_gate, cqubit=6, tqubit=5)
+        qc.start_sub_circuit("AC")
+        measurement_outcomes = qc.measure([12, 5])
+        success = measurement_outcomes[0] == measurement_outcomes[1]
+        if not success:
+            qc.start_sub_circuit("AB")
+            qc.X(13)
+            qc.X(9)
+        qc.start_sub_circuit("B")
+        qc.apply_gate(CNOT_gate, cqubit=9, tqubit=8)
+        qc.start_sub_circuit("D")
+        qc.apply_gate(CNOT_gate, cqubit=2, tqubit=1)
+        qc.start_sub_circuit("BD")
+        measurement_outcomes2 = qc.measure([8, 1], basis="Z")
+        success = measurement_outcomes2[0] == measurement_outcomes2[1]
+        if not success:
             ghz_success = False
             continue
-
+        pbar.update(20) if pbar is not None else None
         pbar.update(20) if pbar is not None else None
 
     # Step 9 from Table D.1 (Thesis Naomi Nickerson)
     # ORDER IS ON PURPOSE: EVERYTIME THE TOP QUBIT IS MEASURED, WHICH DECREASES RUNTIME SIGNIFICANTLY
     qc.start_sub_circuit("B")
-    qc.apply_gate(operation, cqubit=8, tqubit=16)
-    qc.measure(8, probabilistic=False)
+    qc.apply_gate(operation, cqubit=9, tqubit=18)
+    qc.measure(9, probabilistic=False)
 
     qc.start_sub_circuit("A")
-    qc.apply_gate(operation, cqubit=11, tqubit=18)
-    qc.measure(11, probabilistic=False)
+    qc.apply_gate(operation, cqubit=13, tqubit=20)
+    qc.measure(13, probabilistic=False)
 
     qc.start_sub_circuit("D")
-    qc.apply_gate(operation, cqubit=2, tqubit=12)
+    qc.apply_gate(operation, cqubit=2, tqubit=14)
     qc.measure(2, probabilistic=False)
 
     qc.start_sub_circuit("C")
-    qc.apply_gate(operation, cqubit=5, tqubit=14)
-    qc.measure(5, probabilistic=False)
+    qc.apply_gate(operation, cqubit=6, tqubit=16)
+    qc.measure(6, probabilistic=False)
 
     qc.end_current_sub_circuit(total=True)
 
@@ -274,7 +320,7 @@ def dyn_prot_14_1(qc, *, operation, color, save_latex_pdf, pbar, draw_circuit, t
     if save_latex_pdf:
         qc.draw_circuit_latex()
     stab_rep = "Z" if operation == CZ_gate else "X"
-    _, dataframe = qc.get_superoperator([18, 16, 14, 12], stab_rep, no_color=(not color), stabilizer_protocol=True,
+    _, dataframe = qc.get_superoperator([20, 18, 16, 14], stab_rep, no_color=(not color), stabilizer_protocol=True,
                                         print_to_console=to_console, use_exact_path=True)
 
     pbar.update(10) if pbar is not None else None
